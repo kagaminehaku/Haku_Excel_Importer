d3be949fb729289383c9af3393ee5b8f229ee379
Kagamine Haku <blomaway@gmail.com>
2023-05-20 23:52:37 +0700

    Add project files.

---
+++ Haku_Excel_Importer/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Haku_Excel_Importer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.selectfile = new System.Windows.Forms.Button();
+            this.adddata = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // selectfile
+            // 
+            this.selectfile.Location = new System.Drawing.Point(147, 69);
+            this.selectfile.Name = "selectfile";
+            this.selectfile.Size = new System.Drawing.Size(75, 23);
+            this.selectfile.TabIndex = 0;
+            this.selectfile.Text = "Select File";
+            this.selectfile.UseVisualStyleBackColor = true;
+            this.selectfile.Click += new System.EventHandler(this.selectfile_Click);
+            // 
+            // adddata
+            // 
+            this.adddata.Location = new System.Drawing.Point(497, 69);
+            this.adddata.Name = "adddata";
+            this.adddata.Size = new System.Drawing.Size(75, 23);
+            this.adddata.TabIndex = 1;
+            this.adddata.Text = "Add to Database";
+            this.adddata.UseVisualStyleBackColor = true;
+            this.adddata.Click += new System.EventHandler(this.adddata_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.adddata);
+            this.Controls.Add(this.selectfile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button selectfile;
+        private System.Windows.Forms.Button adddata;
+    }
+}
+
---
+++ Haku_Excel_Importer/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+
+namespace Haku_Excel_Importer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string filePath;
+        private List<string> dataColumn1;
+        private List<string> dataColumn2;
+        private List<string> dataColumn3;
+        private void selectfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select an Excel File";
+            openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                Console.WriteLine("File selected: " + filePath);
+            }
+        }
+
+        private void adddata_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Console.WriteLine("Please select an Excel file first.");
+                return;
+            }
+
+            // Read the Excel file
+            using (ExcelPackage package = new ExcelPackage(new System.IO.FileInfo(filePath)))
+            {
+                //ExcelWorksheet worksheet = package.Workbook.Worksheets[1]; // Assuming you want to read the first worksheet
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+
+                // Get the data from the columns
+                int rowCount = worksheet.Dimension.Rows;
+                dataColumn1 = new List<string>();
+                dataColumn2 = new List<string>();
+                dataColumn3 = new List<string>();
+
+                for (int row = 2; row <= rowCount; row++) // Assuming data starts from the second row
+                {
+                    string value1 = worksheet.Cells[row, 1].Value?.ToString(); // Assuming the name is in column 1
+                    string value2 = worksheet.Cells[row, 2].Value?.ToString(); // Assuming the bday is in column 2
+                    string value3 = worksheet.Cells[row, 3].Value?.ToString(); // Assuming the gender is in column 3
+
+                    dataColumn1.Add(value1);
+                    dataColumn2.Add(value2);
+                    dataColumn3.Add(value3);
+                }
+
+                // Insert the data into the database
+                try
+                {
+                    SqlConnection connection = new SqlConnection("Data Source=HATSUNEMIKUPOCK;Initial Catalog=sinhvien;Integrated Security=True");
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("INSERT INTO [profile] (name, bday, gender) VALUES (@Value1, @Value2, @Value3)", connection);
+
+                    for (int i = 0; i < dataColumn1.Count; i++)
+                    {
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@Value1", dataColumn1[i]);
+                        command.Parameters.AddWithValue("@Value2", dataColumn2[i]);
+                        command.Parameters.AddWithValue("@Value3", dataColumn3[i]);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine("Data inserted successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
